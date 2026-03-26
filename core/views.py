376ac7ce7599167232f4d306a42976f8fb72fe4e
J3FF3SON <hb2brito@gmail.com>
2026-03-26 10:31:48 -0300

    Pagina dasboard

--- core/views.py
+++ core/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'home.html')
+
+
+def dashboard_view(request):
+    return render(request, 'dashboard.html')
